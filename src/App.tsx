5816aa04bab940330314cd73df921f99a4c9451d
Vladik <vlad.a.aleynikov@gmail.com>
2023-12-03 19:51:57 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router';
+import { routes } from './routes/index';
+
 function App() {
 
   return (
     <>
+      <Routes>
+        {routes.map((route) =>
+          <Route path={route.path} element={route.element} />)}
+      </Routes>
     </>
   )
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react"
+import Portfolio from "../pages/Portfolio"
+
+interface IRoutes {
+            path: string,
+            element: ReactElement,
+      }
+
+
+export const routes: IRoutes[] = [
+      { path: '/', element: <Portfolio /> }
+]
