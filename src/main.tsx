402a7b2f0c5c57cb1830534304bb937ba3b9e5c6
Vladik <vlad.a.aleynikov@gmail.com>
2023-12-06 19:22:50 +0300

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './style/main.css'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './store/index.ts'
 import { Provider } from 'react-redux'
