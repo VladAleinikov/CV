3120317097321a5b6b04e1234223f805e301d4ba
Vladik <vlad.a.aleynikov@gmail.com>
2023-12-08 16:59:40 +0300

    add nav

--- src/components/Aside/Navigation/index.tsx
+++ src/components/Aside/Navigation/index.tsx
@@ -1,17 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
+  const links = [useRef(null), useRef(null), useRef(null), useRef(null)];
+  const line = useRef(null);
+  const navLeaveHandler = (e: React.MouseEvent<HTMLElement>) => {
+    links.map((link, i) => {
+      link.current.style.transform = "translateX(0px)";
+      if (link.current.classList.contains("active")) {
+        link.current.style.transform = "translateX(10px)";
+        line.current.style.top = i * 38 + 14 + "px";
+      }
+    });
+  };
+  const linkHoverHandler = (e: React.MouseEvent<HTMLLIElement>) => {
+    links.map((link) => (link.current.style.transform = "translateX(0px)"));
+    e.currentTarget.style.transform = "translateX(10px)";
+    line.current.style.top = e.currentTarget.dataset.indexnum * 38 + 14 + "px";
+  };
+  useEffect(() => {
+    links.map((link, i) => {
+      if (link.current.classList.contains("active")) {
+        line.current.style.top = i * 38 + 14 + "px";
+      }
+    });
+  }, [links, line]);
+
   return (
-    <nav className='aside__navigation'>
-      <ul className='navigation__links'>
-        <li className='links__link'><Link to='/about'>Обо мне</Link></li>
-        <li className='links__link'><Link to='/expirience'>Опыт</Link></li>
-        <li className='links__link'><Link to='/education'>Образование</Link></li>
-        <li className='links__link active'><Link to='/projects'>Проекты</Link></li>
+    <nav className="aside__navigation" onMouseLeave={navLeaveHandler}>
+      <span className="navigation__line" ref={line}></span>
+      <ul className="navigation__links">
+        <li
+          data-indexnum="0"
+          ref={links[0]}
+          className="links__link"
+          onMouseEnter={linkHoverHandler}
+        >
+          <Link to="/about">Обо мне</Link>
+        </li>
+        <li
+          data-indexnum="1"
+          ref={links[1]}
+          className="links__link"
+          onMouseEnter={linkHoverHandler}
+        >
+          <Link to="/expirience">Опыт</Link>
+        </li>
+        <li
+          data-indexnum="2"
+          ref={links[2]}
+          className="links__link"
+          onMouseEnter={linkHoverHandler}
+        >
+          <Link to="/education">Образование</Link>
+        </li>
+        <li
+          data-indexnum="3"
+          ref={links[3]}
+          className="links__link active"
+          onMouseEnter={linkHoverHandler}
+        >
+          <Link to="/projects">Проекты</Link>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
