6c0aa52dea84cd84e2f5738b2bb4c5cb67bd759d
Vladik <vlad.a.aleynikov@gmail.com>
2023-12-27 16:17:37 +0300

    add filter functionality

--- src/store/projects/projects.slice.ts
+++ src/store/projects/projects.slice.ts
@@ -16,18 +16,44 @@ const initialState: IProjectsState = {
   },
 };
 
+const filterProjects = (filter: IFilter): Array<IProject> => {
+  return initialState.projects
+    .filter((project) =>
+      filter.technologies === "Всё"
+        ? true
+        : project.technologies.filter((tech) =>
+            filter.technologies.includes(tech)
+          ).length
+    )
+    .filter((project) =>
+      filter.type === "Всё" ? true : project.type === filter.type
+    );
+};
+const sortProjects = ({
+  projects,
+  filter,
+}: IProjectsState): Array<IProject> => {
+  return filter.sort === "По умолчанию"
+    ? initialState.projects
+    : projects.sort((a, b) => {
+        const dateA = new Date(a.date).getTime();
+        const dateB = new Date(b.date).getTime();
+
+        return (filter.sort === "Сначала новые" ? 1 : -1) * (dateB - dateA);
+      });
+};
+
 export const projectsSlice = createSlice({
   name: "projects",
   initialState,
   reducers: {
-    addProject(state, action: PayloadAction<IProject>) {
-      state.projects.push(action.payload);
-    },
     sortProjects(state, action: PayloadAction<SortType>) {
       state.filter.sort = action.payload;
+      state.projects = sortProjects(state);
     },
     filterByType(state, action: PayloadAction<ProjectTypesType>) {
       state.filter.type = action.payload;
+      state.projects = filterProjects(state.filter);
     },
     toggleTechnologies(state, action: PayloadAction<string>) {
       if (action.payload === "Всё") {
@@ -45,8 +71,10 @@ export const projectsSlice = createSlice({
       } else {
         state.filter.technologies = [action.payload];
       }
+      state.projects = filterProjects(state.filter);
     },
     resetFilter(state) {
+      state.projects = initialState.projects;
       state.filter = initialState.filter;
     },
   },
