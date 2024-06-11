a1fba86fd072aa2999f6084552649406c972d24b
Vladik <55589606+VladAleinikov@users.noreply.github.com>
2024-06-11 13:04:58 +0300

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { Hero } from "./_components/hero";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        
+        <FloatingNav
+        navItems={[]}
+        />
+        <Hero/>
       </div>
     </main>
   );
